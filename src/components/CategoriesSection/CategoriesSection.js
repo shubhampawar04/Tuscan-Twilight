a01a038ab96fe7bd045bceb37b15cd7fd1b642d6
shubhampawar04 <pawarshubham81668@gmail.com>
2024-09-06 10:48:30 +0530

    added section file

---
+++ src/components/CategoriesSection/CategoriesSection.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import "./CategoriesSection.css"
+import { Link } from 'react-router-dom';
+
+
+
+function CategoriesSection({ id, name, image,  }) {
+
+  return (
+    <>
+      <Link className="categories-section" to={`${name}`}>
+
+        <div className="categories-container">
+          <img src={image} alt={name} className="category-image" />
+
+          <span className="category-name">{name}</span>
+         
+        </div>
+      </Link>
+
+    </>
+  );
+}
+
+export default CategoriesSection;
